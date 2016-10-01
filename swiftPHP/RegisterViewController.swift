25c06862c5bba4fae52ecdd4dbe805bd7db2dc6c
Jaf Crisologo <rtql8d@gmail.com>
2016-10-01 16:36:42 -0600

    Set up sign in, register and protected pages

---
+++ swiftPHP/RegisterViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RegisterViewController.swift
+//  swiftPHP
+//
+//  Created by Jaf Crisologo on 2016-10-01.
+//  Copyright © 2016 Modium Design. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func cancelBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
