c8da347cb8ac88d2de8a34272d59b861aff3d886
Jaf Crisologo <rtql8d@gmail.com>
2016-10-01 16:52:43 -0600

    Added registration validation

--- swiftPHP/RegisterViewController.swift
+++ swiftPHP/RegisterViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var accountTypeTextField: UITextField!
+    @IBOutlet weak var companyNameTextField: UITextField!
+    @IBOutlet weak var companyPhoneTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var userEmailAddressTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var userPasswordRepeatTextField: UITextField!
+    @IBOutlet weak var userFirstNameTextField: UITextField!
+    @IBOutlet weak var userLastNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,4 +35,32 @@ class RegisterViewController: UIViewController {
     @IBAction func cancelBtnPressed(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    @IBAction func registerBtnPressed(sender: AnyObject) {
+        let userEmail = userEmailAddressTextField.text!
+        let userPassword = userPasswordTextField.text!
+        let userPasswordRepeat = userPasswordRepeatTextField.text!
+        let userFirstName = userFirstNameTextField.text!
+        let userLastName = userLastNameTextField.text!
+        
+        if(userPassword != userPasswordRepeat) {
+            // Display alert message
+            displayAlertMessage("Passwords do not match.")
+            return
+        }
+        
+        if(userEmail.isEmpty || userPassword.isEmpty || userFirstName.isEmpty || userLastName.isEmpty) {
+            // Display alert message
+            displayAlertMessage("All fields must be filled.")
+            return
+        }
+    }
+    
+    func displayAlertMessage(userMessage: String) {
+        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "OK", style:UIAlertActionStyle.Default, handler: nil)
+        myAlert.addAction(okAction)
+        self.presentViewController(myAlert, animated: true, completion: nil)
+    }
+    
 }
